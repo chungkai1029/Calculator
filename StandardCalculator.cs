d4e45fee94b235af3d6aea6cdd1d5292dc28dbe4
chungkai1029 <chungkai1029@gmail.com>
2023-03-05 15:29:00 +0800

    bug fix: decimal operation

--- StandardCalculator.cs
+++ StandardCalculator.cs
@@ -82,18 +82,18 @@ namespace Calculator
         private void buttonEqual_Click(object sender, EventArgs e)
         {
             // Define a regular expression for found operator.
-            Regex regex = new Regex(@"[\p{Sm}\p{Pd}]");
+            Regex findOperator = new Regex(@"[\p{Sm}\p{Pd}]");
 
             // Split the string of textbox with new line character(in Windows).
-            string[] newLineSplit = textBox1.Text.Split("\r\n");
+            string[] splitNewLine = textBox1.Text.Split("\r\n");
 
-            // Check if operator match the Text in textBox1 and the last charactor is "="
-            if (regex.IsMatch(textBox1.Text) && newLineSplit[0].LastIndexOf("=") == -1)
+            // Check if operator match the text in textBox1 and the last charactor is "="
+            if (findOperator.IsMatch(textBox1.Text) && splitNewLine[0].LastIndexOf("=") == -1)
             {
                 // Find the match operator in first line.
-                Match match = regex.Match(newLineSplit[0]);
-                int num1 = Convert.ToInt32(newLineSplit[0].Replace(match.ToString(), ""));
-                int num2 = Convert.ToInt32(newLineSplit[1]);
+                Match match = findOperator.Match(splitNewLine[0]);
+                decimal num1 = Convert.ToDecimal(splitNewLine[0].Replace(match.ToString(), ""));
+                decimal num2 = Convert.ToDecimal(splitNewLine[1]);
 
                 switch (match.ToString())
                 {
@@ -111,24 +111,25 @@ namespace Calculator
                         break;
                 }
             }
-            else if (regex.IsMatch(textBox1.Text))
+            else if (findOperator.IsMatch(textBox1.Text))
             {
-                // Find the match word in first line.
-                Match match = regex.Match(newLineSplit[0]);
+                // Find the match words in first line and get the fitst match operator.
+                MatchCollection matches = findOperator.Matches(splitNewLine[0]);
+                Match firstMatch = matches[0];
 
-                // If the match word is "=" then keep text on textbox.
-                if (match.ToString() == "=")
+                // If the first match word is "=" then keep text on textbox.
+                if (firstMatch.ToString() == "=")
                 {
-                    textBox1.Text = newLineSplit[0].Replace("=", "") + buttonEqual.Text + "\r\n" + newLineSplit[1];
+                    textBox1.Text = splitNewLine[0] + "\r\n" + splitNewLine[1];
                     return;
                 }
 
                 // Split first line with operator (exclude equal) to get the second number.
-                string[] operatorSplit = newLineSplit[0].Replace("=", "").Split(match.ToString());
-                int num1 = Convert.ToInt32(newLineSplit[1]);
-                int num2 = Convert.ToInt32(operatorSplit[1]);
+                string[] splitOperator = splitNewLine[0].Replace("=", "").Split(firstMatch.ToString());
+                decimal num1 = Convert.ToDecimal(splitNewLine[1]);
+                decimal num2 = Convert.ToDecimal(splitOperator[1]);
 
-                switch (match.ToString())
+                switch (firstMatch.ToString())
                 {
                     case "+":
                         textBox1.Text = $"{num1}+{num2}" + buttonEqual.Text + "\r\n" + Convert.ToString(num1 + num2);
@@ -153,18 +154,18 @@ namespace Calculator
         private void buttonPlus_Click(object sender, EventArgs e)
         {
             // Define a regular expression for found operator.
-            Regex regex = new Regex(@"[\p{Sm}\p{Pd}]");
+            Regex findOperator = new Regex(@"[\p{Sm}\p{Pd}]");
 
             // Split the string of textbox with new line character(in Windows).
-            string[] newLineSplit = textBox1.Text.Split("\r\n");
+            string[] splitNewLine = textBox1.Text.Split("\r\n");
 
             // Check if operator match the Text in textBox1 and the last charactor is "="
-            if (regex.IsMatch(textBox1.Text) && newLineSplit[0].LastIndexOf("=") == -1)
+            if (findOperator.IsMatch(textBox1.Text) && splitNewLine[0].LastIndexOf("=") == -1)
             {
                 // Find the match operator in first line.
-                Match match = regex.Match(newLineSplit[0]);
-                int num1 = Convert.ToInt32(newLineSplit[0].Replace(match.ToString(), ""));
-                int num2 = Convert.ToInt32(newLineSplit[1]);
+                Match match = findOperator.Match(splitNewLine[0]);
+                decimal num1 = Convert.ToDecimal(splitNewLine[0].Replace(match.ToString(), ""));
+                decimal num2 = Convert.ToDecimal(splitNewLine[1]);
 
                 switch (match.ToString())
                 {
@@ -182,60 +183,31 @@ namespace Calculator
                         break;
                 }
             }
-            else if (regex.IsMatch(textBox1.Text))
+            else if (findOperator.IsMatch(textBox1.Text))
             {
-                // Find the match word in first line.
-                Match match = regex.Match(newLineSplit[0]);
-
-                // If the match word is "=" then keep text on textbox.
-                if (match.ToString() == "=")
-                {
-                    textBox1.Text = newLineSplit[0].Replace("=", "") + buttonPlus.Text + "\r\n" + newLineSplit[1];
-                    return;
-                }
-
-                // Split first line with operator (exclude equal) to get the second number.
-                string[] operatorSplit = newLineSplit[0].Replace("=", "").Split(match.ToString());
-                int num1 = Convert.ToInt32(newLineSplit[1]);
-                int num2 = Convert.ToInt32(operatorSplit[1]);
-
-                switch (match.ToString())
-                {
-                    case "+":
-                        textBox1.Text = $"{num1}+{num2}" + buttonEqual.Text + "\r\n" + Convert.ToString(num1 + num2);
-                        break;
-                    case "-":
-                        textBox1.Text = $"{num1}-{num2}" + buttonEqual.Text + "\r\n" + Convert.ToString(num1 - num2);
-                        break;
-                    case "กั":
-                        textBox1.Text = $"{num1}กั{num2}" + buttonEqual.Text + "\r\n" + Convert.ToString(num1 * num2);
-                        break;
-                    case "กา":
-                        textBox1.Text = $"{num1}กา{num2}" + buttonEqual.Text + "\r\n" + Convert.ToString(num1 / num2);
-                        break;
-                }
+                textBox1.Text = splitNewLine[1] + buttonPlus.Text + "\r\n" + splitNewLine[1];
             }
             else
             {
-                textBox1.Text += buttonEqual.Text + "\r\n" + textBox1.Text;
+                textBox1.Text += buttonPlus.Text + "\r\n";
             }
         }
 
         private void buttonMinus_Click(object sender, EventArgs e)
         {
             // Define a regular expression for found operator.
-            Regex regex = new Regex(@"[\p{Sm}\p{Pd}]");
+            Regex findOperator = new Regex(@"[\p{Sm}\p{Pd}]");
 
-            // Find if the string of textbox match or not.
-            if (regex.IsMatch(textBox1.Text))
+            // Split the string of textbox with new line character(in Windows).
+            string[] splitNewLine = textBox1.Text.Split("\r\n");
+
+            // Check if operator match the Text in textBox1 and the last charactor is "="
+            if (findOperator.IsMatch(textBox1.Text) && splitNewLine[0].LastIndexOf("=") == -1)
             {
-                // Split the string of textbox with new Line.
-                string[] newLineSplit = textBox1.Text.Split("\r\n");
-
-                // Find the match word.
-                Match match = regex.Match(newLineSplit[0]);
-                int num1 = Convert.ToInt32(newLineSplit[0].Replace(match.ToString(), ""));
-                int num2 = Convert.ToInt32(newLineSplit[1]);
+                // Find the match operator in first line.
+                Match match = findOperator.Match(splitNewLine[0]);
+                decimal num1 = Convert.ToDecimal(splitNewLine[0].Replace(match.ToString(), ""));
+                decimal num2 = Convert.ToDecimal(splitNewLine[1]);
 
                 switch (match.ToString())
                 {
@@ -253,6 +225,10 @@ namespace Calculator
                         break;
                 }
             }
+            else if (findOperator.IsMatch(textBox1.Text))
+            {
+                textBox1.Text = splitNewLine[1] + buttonMinus.Text + "\r\n" + splitNewLine[1];
+            }
             else
             {
                 textBox1.Text += buttonMinus.Text + "\r\n";
@@ -262,18 +238,18 @@ namespace Calculator
         private void buttonMultiple_Click(object sender, EventArgs e)
         {
             // Define a regular expression for found operator.
-            Regex regex = new Regex(@"[\p{Sm}\p{Pd}]");
+            Regex findOperator = new Regex(@"[\p{Sm}\p{Pd}]");
 
-            // Find if the string of textbox match or not.
-            if (regex.IsMatch(textBox1.Text))
+            // Split the string of textbox with new line character(in Windows).
+            string[] splitNewLine = textBox1.Text.Split("\r\n");
+
+            // Check if operator match the Text in textBox1 and the last charactor is "="
+            if (findOperator.IsMatch(textBox1.Text) && splitNewLine[0].LastIndexOf("=") == -1)
             {
-                // Split the string of textbox with new Line.
-                string[] newLineSplit = textBox1.Text.Split("\r\n");
-
-                // Find the match word.
-                Match match = regex.Match(newLineSplit[0]);
-                int num1 = Convert.ToInt32(newLineSplit[0].Replace(match.ToString(), ""));
-                int num2 = Convert.ToInt32(newLineSplit[1]);
+                // Find the match operator in first line.
+                Match match = findOperator.Match(splitNewLine[0]);
+                decimal num1 = Convert.ToDecimal(splitNewLine[0].Replace(match.ToString(), ""));
+                decimal num2 = Convert.ToDecimal(splitNewLine[1]);
 
                 switch (match.ToString())
                 {
@@ -291,6 +267,10 @@ namespace Calculator
                         break;
                 }
             }
+            else if (findOperator.IsMatch(textBox1.Text))
+            {
+                textBox1.Text = splitNewLine[1] + buttonMultiple.Text + "\r\n" + splitNewLine[1];
+            }
             else
             {
                 textBox1.Text += buttonMultiple.Text + "\r\n";
@@ -300,18 +280,18 @@ namespace Calculator
         private void buttonDivide_Click(object sender, EventArgs e)
         {
             // Define a regular expression for found operator.
-            Regex regex = new Regex(@"[\p{Sm}\p{Pd}]");
+            Regex findOperator = new Regex(@"[\p{Sm}\p{Pd}]");
 
-            // Find if the string of textbox match or not.
-            if (regex.IsMatch(textBox1.Text))
+            // Split the string of textbox with new line character(in Windows).
+            string[] splitNewLine = textBox1.Text.Split("\r\n");
+
+            // Check if operator match the Text in textBox1 and the last charactor is "="
+            if (findOperator.IsMatch(textBox1.Text) && splitNewLine[0].LastIndexOf("=") == -1)
             {
-                // Split the string of textbox with new Line.
-                string[] newLineSplit = textBox1.Text.Split("\r\n");
-
-                // Find the match word.
-                Match match = regex.Match(newLineSplit[0]);
-                int num1 = Convert.ToInt32(newLineSplit[0].Replace(match.ToString(), ""));
-                int num2 = Convert.ToInt32(newLineSplit[1]);
+                // Find the match operator in first line.
+                Match match = findOperator.Match(splitNewLine[0]);
+                decimal num1 = Convert.ToDecimal(splitNewLine[0].Replace(match.ToString(), ""));
+                decimal num2 = Convert.ToDecimal(splitNewLine[1]);
 
                 switch (match.ToString())
                 {
@@ -329,6 +309,10 @@ namespace Calculator
                         break;
                 }
             }
+            else if (findOperator.IsMatch(textBox1.Text))
+            {
+                textBox1.Text = splitNewLine[1] + buttonDivide.Text + "\r\n" + splitNewLine[1];
+            }
             else
             {
                 textBox1.Text += buttonDivide.Text + "\r\n";
@@ -339,10 +323,13 @@ namespace Calculator
         private void buttonDecimal_Click(object sender, EventArgs e)
         {
             // Define a regular expression for found decimal.
-            Regex regex = new Regex("\\.");
+            Regex findDecimal = new Regex("\\.");
+
+            // Split the string of textbox with new line character(in Windows).
+            string[] splitNewLine = textBox1.Text.Split("\r\n");
 
             // Find if the string of textbox match or not.
-            if (regex.IsMatch(textBox1.Text))
+            if (findDecimal.IsMatch(splitNewLine[splitNewLine.Length - 1]))
             {
                 return;
             }
